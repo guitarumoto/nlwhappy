6061e95621cf8ec8b60f1f4ffa5db306f4e852ce
guitarumoto <guitarumoto@gmail.com>
2020-12-07 18:11:08 -0400

    Inserindo mapa do MapBox

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -3,7 +3,8 @@ import mapMarkerImg from '../images/map-marker.svg';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
 import '../styles/pages/orphanages-map.css';
-
+import { MapContainer, TileLayer} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 
 function OrphanagesMap(){
@@ -20,6 +21,10 @@ function OrphanagesMap(){
                     <span>Mato Grosso do Sul</span> 
                 </footer>
             </aside>
+            
+            <MapContainer center={[-20.4919382,-54.59853]} zoom={15} style={{ width:'100%', height:'100%' }}>
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+            </MapContainer>
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
