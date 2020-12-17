58fabc487408d554187b80870946e2fd8db8dde6
guitarumoto <guitarumoto@gmail.com>
2020-12-17 19:27:43 -0400

    Estruturando MVC

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import './database/connection'
+import routes from './routes'
 
 const app = express();
 
-app.listen(3333)
+app.use(express.json());
+app.use(routes);
 
-// REQUISIÇÃO / RESPOSTA
+
+
+app.listen(3333);
